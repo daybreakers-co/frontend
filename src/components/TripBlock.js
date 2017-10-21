1398e2e0141a3594c8d3517f1618040dee927ea5
Robert Beekman <robert@matsimitsu.nl>
2017-10-21 13:51:35 +0200

    Make TripBlock work

--- src/components/TripBlock.js
+++ src/components/TripBlock.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { propType } from 'graphql-anywhere';
+import { propType } from 'graphql-anywhere'
 import PropTypes from 'prop-types'
+import pluralize from 'pluralize'
 
 import { Link } from 'react-router-dom'
 import ScaledImage from './ScaledImage'
@@ -8,52 +9,65 @@ import './TripBlock.css'
 
 import TripBlockFragment from '../graphql/_TripBlock.gql'
 
-const TripBlock = ({ link, trip: { title, subtitle, header } }) => (
-  <div className="TripBlock">
+const TripBlock = ({ link, trip: { title, subtitle, header, posts, photos } }) => {
+
+  let shuffled = shuffle(photos)
+  let photoGroups = partition(shuffled, 3, 8)
+  return(<div className="TripBlock">
     <div className="Container">
       <header>
         <h1>
           <Link to={link}>
             {title || "Untitled"}
-            <small>25 days &mdash; 3 posts &mdash; 234 photos</small>
+            <small>
+              25 days
+              &mdash;
+              {`${posts.length} ${pluralize("post", posts.length)}`}
+              &mdash;
+              {`${photos.length} ${pluralize("photo", photos.length)}`}
+            </small>
           </Link>
         </h1>
         {subtitle && <p>{subtitle}</p>}
         <Link to={link} className="Button large secondary">View trip</Link>
       </header>
       <div className="images">
-        <div>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-        </div>
-        <div>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-        </div>
-        <div>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-          <figure><img src={`${header.url}?width=360`} /></figure>
-        </div>
+        {photoGroups.map(group => (
+          <div key={group.map(p => p.id).join()}>
+            {group.map(photo => (
+              <figure key={photo.id}><img src={`${photo.url}?width=360`} /></figure>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
-  </div>
-)
+  </div>)
+}
+
+function partition(input, size, max) {
+  var results = [];
+  for (var i = 0; i < size; i += 1) {
+    results[i] = []
+  }
+  for (var i = 0; (i < input.length ); i += 1) {
+    if (max && i == size * max) { break }
+    results[i % size].push(input[i])
+  }
+  return results;
+}
+
+function shuffle(input) {
+  var output = input.slice(0)
+  for (var i = output.length - 1; i >= 0; i--) {
+
+    var randomIndex = Math.floor(Math.random() * (i + 1));
+    var itemAtIndex = output[randomIndex];
+
+    output[randomIndex] = output[i];
+    output[i] = itemAtIndex;
+  }
+  return output;
+}
 
 TripBlock.propTypes = {
   link: PropTypes.string.isRequired,
