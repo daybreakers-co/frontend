ec0fe17c069dfcf5cf35b04cace52c269108e661
wesoudshoorn <wes@appsignal.com>
2017-10-25 00:07:31 +0200

    Refactor loading

--- src/pages/DeleteTripPage.js
+++ src/pages/DeleteTripPage.js
@@ -4,6 +4,7 @@ import { graphql, compose } from 'react-apollo'
 import Button from '../components/Button'
 
 import Modal from '../components/Modal'
+import LoadingPage from '../components/LoadingPage'
 
 import DeleteTripPageQuery from '../graphql/DeleteTripPageQuery.gql'
 import DeleteTripQuery from '../graphql/DeleteTripQuery.gql'
@@ -37,7 +38,7 @@ class DeleteTripPage extends Component {
   render() {
     const { data: { loading, error, user } } = this.props;
     if (error)   { return (<div>ERROR: {error}</div>) }
-    if (loading) { return (<div>Loading...</div>)     }
+    if (loading) { return (<LoadingPage />) }
 
     return (
       <Modal onClose={this.back}>
